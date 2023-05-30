11276a99b9f8b80fd8298ac14d974c960c77cd93
Aaron Otto <122137616+ottoaara@users.noreply.github.com>
2023-05-30 18:41:49 -0500

    Add files via upload

---
+++ SQL Scripts for challenge .sql
@@ -0,0 +1,83 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Mnxh2g
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- Module 9 ERD Aaron Otto
+-- please note changed all columns to
+-- varchar in actual data model
+-- from string
+CREATE TABLE "departmerts" (
+    "dept_no" string(8)NOT   NULL,
+    "dept_name" string1(100)NOT   NULL,
+    CONSTRAINT "pk_departmerts" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" string   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+-- PRIMARY KEY(emp_no, dept_no)
+CREATE TABLE "dept_manager" (
+    -- PRIMARY KEY (dept_no, emp_no)
+    "dept_no" string   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" intNOT   NULL,
+    "emp_title_id" string   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" string(100)NOT   NULL,
+    "last_name" string(100)NOT   NULL,
+    "sex" string(1)   NOT NULL,
+    "hire_date" dateNOT   NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" money   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no","salary"
+     )
+);
+
+-- PRIMARY KEY(emp_no, salary)
+CREATE TABLE "titles" (
+    "title_id" string   NOT NULL,
+    "title" string(100)NOT   NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departmerts" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departmerts" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
